3ce19dd8df579ad00fb7e6c97c6799496051cf75
Alexander Gude <alex.public.account@gmail.com>
2021-01-03 12:35:02 -0800

    Add all 81 possible dice rolls

---
+++ _code/make_dice_macros.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from itertools import product
+
+DIE_FACES = {
+    "p": "{{pos}}",
+    "n": "{{neg}}",
+    "b": "{{blank}}",
+}
+
+output = []
+for combo in product(DIE_FACES.keys(), repeat=4):
+    key = f"dice-{''.join(combo)}"
+    value = ""
+    for char in combo:
+        value += DIE_FACES[char]
+
+    line = f"{{% capture {key} %}}{value}{{% endcapture %}}"
+    print(line)
